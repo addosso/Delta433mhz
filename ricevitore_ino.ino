8b745d61d9931ad197aefc79d87b19c5b2ef613b
addosso <rosa.1610889@studenti.uniroma1.it>
2016-07-20 23:08:42 +0200

    Update ricevitore_ino.ino

--- ricevitore_ino.ino
+++ ricevitore_ino.ino
@@ -15,7 +15,7 @@ void setup()
   
    
     vw_set_rx_pin(RX_pin); //Non servirebbe: il default è 11
-    vw_setup(3000);        // Bits per sec
+    vw_setup(1000);        // Bits per sec
     vw_rx_start();         // Inizio il processo di ricezione
     servoSx.attach(SERVO_MOTOR_SX);
     servoDx.attach(SERVO_MOTOR_DX);
@@ -35,6 +35,7 @@ void loop()
     uint8_t buflen = VW_MAX_MESSAGE_LEN;
     if (vw_get_message(buf, &buflen)) // Non-blocking
     {
+      Serial.println("azz");
       
 	buf[buflen]='\0'; // Metto il terminatore di stringa
         if (strncmp((char *)buf,"MOTOR_ON",buflen)==0){
@@ -43,23 +44,28 @@ void loop()
         if (strncmp((char *)buf,"MOTOR_OFF",buflen)==0){
              digitalWrite(RELAY,LOW);    
         }
-        if(strncmp((char *)buf, "CABRATA", buflen)==0){
-            servoSx.write(2000);
+        if(strncmp((char *)buf, "CAB", buflen)==0){
+           servoSx.write(2000);
             servoDx.write(2000);
+            Serial.println("cabrata attivo: ");
         }          
-        if(strncmp((char *)buf, "PICCHIATA", buflen)==0){
+        if(strncmp((char *)buf, "PIC", buflen)==0){
             servoSx.write(900);
            servoDx.write(900);
+           Serial.println("picchiata ");
         }
         if(strncmp((char *)buf, "DX", buflen)==0){
           servoSx.write(2000);
+          Serial.println("dx: ");
         } 
         if(strncmp((char *)buf, "SX", buflen)==0){
           servoDx.write(2000);
+          Serial.println("sx ");
         }
         if(strncmp((char *)buf, "STAY", buflen)==0){
            servoDx.write(1400);
            servoSx.write(1400);
+           Serial.println("stay: ");
         }
       }  
 }
